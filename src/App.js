99cb88160db2718d6853a91867b29fa690d55270
Marko Srdoč <markosrdoc@gmail.com>
2019-07-12 15:20:26 +0200

    Initial state
    Initial state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Movies component here
+import Movies from './Components/Movies';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: false
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Movies />
+      </div>
+    );
+  }
 }
+
+
+// class App extends Component {
+//   constructor(props) {    
+//     super(props);
+//     this.state = {  
+//           show: false  
+//     };
+//   }
+  
+  // render() {  
+  //     // use Todo component inside render method.    
+  //     return (
+  //       <div className="App">
+  //         <Movies />
+  //         </div>
+  //         );
+  //       }}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   constructor(props) {    
+//     super(props);
+//     this.state = {  
+//           show: false  
+//     };
+//   }
+//   return (
+//     render() {  
+//       // use Todo component inside render method.    
+//       return (
+//         <div className="App">
+//           <Movies />
+//           </div>
+//           );
+//         }
+//     )
+// }
 
 export default App;
---
+++ src/Components/Movies.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Movies extends Component {     
+    render() {
+        return(<h1>This message is from Movies component</h1>)
+    }
+}
+
+export default Movies;
